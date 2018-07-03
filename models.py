562620225d7fc5bd98e677ee91111b0027124fa8
pgunnink <38908229+pgunnink@users.noreply.github.com>
2018-07-03 17:02:12 +0200

    Added more model options

--- models.py
+++ models.py
@@ -9,7 +9,14 @@ def baseModelDense(N_stations=1,
                    features=2,
                    length_trace=80,
                    trace_filter_1=64,
-                   trace_filter_2=32):
+                   filter_size_1=7,
+                   stride_1=4,
+                   trace_filter_2=32,
+                   filter_size_2=7,
+                   stride_2=4,
+                   trace_filter_3=10,
+                   filter_size_3=4,
+                   stride_3=4,):
     '''
     The basic model as described by https://arxiv.org/pdf/1708.00647.pdf, but flattened
     out
@@ -22,16 +29,19 @@ def baseModelDense(N_stations=1,
 
     process_metadata = Reshape((N_stations, 4, features))(input_metadata)
 
-    Trace = Conv2D(trace_filter_1, (1, 7), strides=(1, 4), padding='valid',
+    Trace = Conv2D(trace_filter_1, (1, filter_size_1), strides=(1, stride_1), padding='valid',
                    activation='relu', data_format='channels_last',
                    kernel_initializer='he_normal', name='first_trace_conv')(
         reshape_traces)
-    Trace = Conv2D(trace_filter_2, (1, 7), strides=(1, 4), padding='valid',
+    Trace = Conv2D(trace_filter_2, (1, filter_size_2), strides=(1, stride_2),
+                   padding='valid',
                    activation='relu', kernel_initializer='he_normal',
                    name='second_trace_conv' )(Trace)
-    Trace = Conv2D(10, (1, 4), strides=(1, 1), padding='valid', activation='relu',
+    Trace = Conv2D(trace_filter_3, (1, filter_size_3), strides=(1, stride_3),
+                   padding='valid',
+                   activation='relu',
                    kernel_initializer='he_normal', name='third_trace_conv')(Trace)
-    TraceResult = Reshape((N_stations, 4, 10))(Trace)
+    TraceResult = Reshape((N_stations, 4, -1))(Trace)
 
     x = concatenate([TraceResult, process_metadata])
 
